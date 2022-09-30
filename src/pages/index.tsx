da858c82bd47a4d3179340c6572bb9e1b6e25c88
Robert Dalmacio <rjohndalmacio@hotmail.com>
2022-09-30 15:51:16 -0400

    Created Users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import type { NextPage } from 'next'
+import {signIn, signOut, useSession} from 'next-auth/react'
 
 const Home: NextPage = () => {
+  const {data} = useSession()
+
   return (
-    <div>Hello Home Page</div>
+    <div>
+      {data?.user ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn("google")}>Sign In</button>
+      )}
+      {data?.user?.name}
+    </div>
   )
 }
 
