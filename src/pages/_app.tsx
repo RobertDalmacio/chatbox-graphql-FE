0e8ba3a65349420846e96d7e0163e00cdcadf40b
Robert Dalmacio <rjohndalmacio@hotmail.com>
2022-10-01 14:14:15 -0400

    Apollo Client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,20 @@
-import {ChakraProvider} from '@chakra-ui/react'
+import { ApolloProvider } from '@apollo/client'
+import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
-import {theme} from "../chakra/theme"
+import { theme } from "../chakra/theme"
+import { client } from '../graphql/apollo-client'
 
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps} }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
+    </ApolloProvider>
   )
 }
 
