38bc4da86f69819e2eddf692f1945fc9ea96a7d3
Robert Dalmacio <rjohndalmacio@hotmail.com>
2022-10-03 15:38:30 -0400

    session ts error fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import { ApolloProvider } from '@apollo/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
+import { Session } from "next-auth"
 import type { AppProps } from 'next/app'
 import { theme } from "../chakra/theme"
 import { client } from '../graphql/apollo-client'
 
 
-function MyApp({ Component, pageProps: { session, ...pageProps} }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
     <ApolloProvider client={client}>
-      <SessionProvider session={session}>
+      <SessionProvider session={pageProps.session}>
         <ChakraProvider theme={theme}>
           <Component {...pageProps} />
         </ChakraProvider>
